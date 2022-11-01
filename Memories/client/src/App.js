55bf13386b20c36d5ee1fe2c8fa49bebb5e1050f
meankit2610 <ankitsingh5282770@gmail.com>
2022-11-02 00:15:04 +0530

    Added Redux,Form-created,Structure is made

--- Memories/client/src/App.js
+++ Memories/client/src/App.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React, {useEffect} from "react"
+import {Container, AppBar, Typography, Grow, Grid} from "@mui/material"
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
+import {getPosts} from './actions/posts'
+import { useDispatch } from 'react-redux';
+const App = ()=> {
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-
-function App() {
+  useEffect(() => {
+    dispatch(getPosts());
+  },[dispatch])
   return (
-    <div>
-      <h2>App</h2>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography  variant="h2" align="center">Memories</Typography>
+        <img  src={memories} alt="memories-alt" height="60" ></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} >
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ Memories/client/src/styles.js
@@ -0,0 +1,20 @@
+// import { makeStyles } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+
+export default makeStyles(() => ({
+     appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgb(255, 0, 0)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
+
